533daa54d03ef00c6543493ab073ba596d77d8fb
YoungProducer <sashabezrukovownmail@gmail.com>
2020-08-04 09:18:09 +0300

    fix: added module name mapping to support absolute imports in tests.

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersService } from '../users/users.service';
+import { UsersService } from 'src/users/users.service';
 
 describe('Auth Controller', () => {
   let controller: AuthController;
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { AuthController } from './auth.controller';
-import { UsersService } from '../users/users.service';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [AuthController],
