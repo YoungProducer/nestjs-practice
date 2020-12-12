d122e6054e19f8d6562c09a281b4b0b7c4c78ef5
YoungProducer <sashabezrukovownmail@gmail.com>
2020-12-13 00:54:55 +0200

    fixed auth service test.

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
-import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
 import { PasswordHasherModule } from 'src/password-hasher/password-hasher.module';
+import { MockedUsersModule } from 'src/__mocks__/modules/users.module.mock';
+import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
     let service: AuthService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [UsersModule, PasswordHasherModule],
+            imports: [MockedUsersModule, PasswordHasherModule],
             providers: [AuthService],
         }).compile();
 
